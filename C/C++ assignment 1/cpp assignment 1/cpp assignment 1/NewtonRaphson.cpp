6464047c53688e994dc102e4c42409ec22c0c305
knutlo <lo.knut@gmail.com>
2013-11-01 17:48:28 +0100

    corrected source file headers (it said C assignment 4 instead of C++ assignment 1)

--- C/C++ assignment 1/cpp assignment 1/cpp assignment 1/NewtonRaphson.cpp
+++ C/C++ assignment 1/cpp assignment 1/cpp assignment 1/NewtonRaphson.cpp
@@ -1,11 +1,10 @@
 // Uppsala University
 // Department Of IT
 // Programming bridging course Autumn 2013
-// C assignment 4
+// C++ assignment 1
 // Student: Knut Lorenzen 810326-T296
 //
 //  NewtonRaphson.cpp
-//  cpp assignment 1
 //
 //  Created by Knut Lorenzen on 31/10/2013.
 //  Copyright (c) 2013 Knut Lorenzen. All rights reserved.
